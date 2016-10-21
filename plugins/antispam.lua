b4b1e24abcfdd7ab0ba5ee3a790db89dfe916c80
claydev <saad8kw@gmail.com>
2016-10-21 23:52:58 +0300

    Update antispam.lua

--- plugins/antispam.lua
+++ plugins/antispam.lua
@@ -9,16 +9,16 @@ if not(msg.chat.type == 'private') and not is_mod(msg) then
     math.randomseed(os.time());
  	var = math.random(0,4);
     if var == 0 then
-        api.sendMessage(msg.chat.id, 'Hey *' ..name.. '* ID '..iduser..', No envies enlaces de otros grupos o serás expulsado', true)
+        api.sendMessage(msg.chat.id, 'Hey *' ..name.. '* ID '..iduser..', Do not send links from other groups or you will be expelled', true)
 	elseif var == 1 then
- 	   	api.sendMessage(msg.chat.id, 'Recuerda *' ..name.. '* ID '..iduser..', si envias enlaces de otros grupos serás expulsado', true)
+ 	   	api.sendMessage(msg.chat.id, 'Recuerda *' ..name.. '* ID '..iduser..', If you send links from other groups you will be expelled', true)
 	elseif var == 2 then
- 	   	api.sendMessage(msg.chat.id, 'Lo siento, *' ..name.. '*, ID '..iduser..', expulsado por spammer', true)
+ 	   	api.sendMessage(msg.chat.id, 'Lo siento, *' ..name.. '*, ID '..iduser..', expelled for spammer', true)
  	   api.kickUser(msg.chat.id, msg.from.id)
 	elseif var == 3 then
-  	   	api.sendMessage(msg.chat.id, '*' ..name.. '* ID '..iduser..', si sigues enviando spam serás baneado definitivamente', true)
+  	   	api.sendMessage(msg.chat.id, '*' ..name.. '* ID '..iduser..', if you keep sending spam you will be definitely banned', true)
   	elseif var == 4 then
-  	   	api.sendMessage(msg.chat.id, 'Lo siento *' ..name.. '*, ID '..iduser..', baneado por spammer.', true)
+  	   	api.sendMessage(msg.chat.id, 'Lo siento *' ..name.. '*, ID '..iduser..', banned for spammer.', true)
  	   api.kickChatMember(msg.chat.id, msg.from.id)
   end
 end
