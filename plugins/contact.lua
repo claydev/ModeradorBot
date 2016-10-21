71e5ed5f189f99cbf80d7b5da94f203de0ab5848
claydev <saad8kw@gmail.com>
2016-10-21 23:12:02 +0300

    Update contact.lua

--- plugins/contact.lua
+++ plugins/contact.lua
@@ -11,17 +11,17 @@ local action = function(msg, blocks)
     idto = blocks[3]
     message = blocks[2]
     nameowner = msg.from.first_name
-	api.sendMessage(idto, message.. '\n\nEnviado por: ' ..nameowner.. '\n_Contesta con /responder [respuesta]_', true)
-	api.sendMessage(msg.chat.id, '¡Mensaje enviado!')
+	api.sendMessage(idto, message.. '\n\nSent by: ' ..nameowner.. '\n_Answer with / answer [reply]_', true)
+	api.sendMessage(msg.chat.id, 'Message sent!')
 end
  if blocks[1] == 'responder' then  
  	iduser = msg.from.id
   name = msg.from.first_name
   user = msg.from.username
  	message = blocks[2]
- 	api.sendMessage(config.admin, make_text'' ..message.. '\n\n_ID Del Usuario_: ' ..iduser.. '\n_Nombre_: ' ..name.. '\n_Usuario_: @' ..user, true)
- 	api.sendMessage(config.support, make_text'' ..message.. '\n\n_ID Del Usuario_: ' ..iduser.. '\n_Nombre_: ' ..name.. '\n_Usuario_: @' ..user, true)
- 	api.sendMessage(msg.chat.id, '¡Respuesta enviada!')
+ 	api.sendMessage(config.admin, make_text'' ..message.. '\n\n_ID_: ' ..iduser.. '\n_Name_: ' ..name.. '\n_User_: @' ..user, true)
+ 	api.sendMessage(config.support, make_text'' ..message.. '\n\n_ID_: ' ..iduser.. '\n_Name_: ' ..name.. '\n_User_: @' ..user, true)
+ 	api.sendMessage(msg.chat.id, 'Reply sent!')
  	end
 -- if blocks[1] == 'duda' or blocks[1] == 'ayuda' or blocks[1] == 'soporte' then
 --    api.sendMessage(config.support, message.. '\n\n_ID Del Usuario:_ ' ..iduser, true)
