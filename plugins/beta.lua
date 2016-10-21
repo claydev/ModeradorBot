82c5edc32de63ecdc93deeb5278661cc013cca1b
claydev <saad8kw@gmail.com>
2016-10-21 23:06:16 +0300

    Update beta.lua

--- plugins/beta.lua
+++ plugins/beta.lua
@@ -7,7 +7,7 @@ local action = function(msg, blocks, ln)
 if blocks[1] == 'beta' then
 	username = msg.from.username
     	api.sendDocument(msg.from.id, './org.telegram.plus-3.10.1.0.apk')
-    	api.sendMessage(msg.chat.id, 'Te enviaré la versión beta última de Plus Messenger en un minuto por privado.\n*Verifica el privado del bot* :)\n\nBeta enviada a: @' ..username, true)
+    	api.sendMessage(msg.chat.id, 'Keeper Bot Best Bot In TeleGram.\n*BOT BY SAADTHAMER* :)\n\nBeta a: @' ..username, true)
     end
     end
     
