f71da6a9f98c8ee26992644ab4bac28d1552d01c
claydev <saad8kw@gmail.com>
2016-10-22 00:43:47 +0300

    Update gbanner.lua

--- plugins/gbanner.lua
+++ plugins/gbanner.lua
@@ -5,17 +5,17 @@ local action = function(msg, matches)
 if matches[1] == "gban" then
 	if matches[2] then
 		os.execute('echo "' ..matches[2].. '," >> ./data/gbans')
-		api.sendReply(msg, "ID " ..matches[2].. " *globalmente baneado*.", true)
+		api.sendReply(msg, "ID " ..matches[2].. " *globally banned*.", true)
 		bot_init(true)
  	end
  
  	if not matches[2] then
 		if msg.reply then
 			os.execute('echo "' ..msg.reply.from.id.. '," >> ./data/gbans')
-			api.sendReply(msg, "ID " ..msg.reply.from.id.. " *globalmente baneado*.", true)
+			api.sendReply(msg, "ID " ..msg.reply.from.id.. " *globally banned*.", true)
 			bot_init(true)
 		else
-			api.sendMessage(msg.chat.id, "Este comando necesita respuesta")
+			api.sendMessage(msg.chat.id, "This command needs to answer")
 		end
 	end
 end
@@ -24,17 +24,17 @@ if matches[1] == "ungban" then
 	
  	if matches[2] then
 		os.execute('sed -i "/' ..matches[2].. '/d" ./data/gbans')
-		api.sendReply(msg, "ID " ..matches[2].. " *globalmente desbaneado*.", true)
+		api.sendReply(msg, "ID " ..matches[2].. " *globally unbanned*.", true)
 		bot_init(true)
 	end
 	
 	 if not matches[2] then
 		if msg.reply then
 			os.execute('sed -i "/' ..msg.reply.from.id.. '/d" ./data/gbans')
-			api.sendReply(msg, "ID " ..msg.reply.from.id.. " *globalmente desbaneado*.", true)
+			api.sendReply(msg, "ID " ..msg.reply.from.id.. " *globally unbanned*.", true)
 			bot_init(true)
 		else
-			api.sendMessage(msg.chat.id, "Este comando necesita respuesta")
+			api.sendMessage(msg.chat.id, "This command needs to answer")
 		end
  	 end
 end
@@ -43,10 +43,10 @@ if matches[1] == "isban" then
 
 	 if matches[2] then
 			os.execute('grep -o "' ..matches[2].. '" ./data/gbans')
-			api.sendReply(msg, "ID " ..matches[2].. " *ID se encuentra en BD*.", true)
+			api.sendReply(msg, "ID " ..matches[2].. " *ID is in BD*.", true)
 			else
 			os.execute('grep -o "' ..matches[2].. '" ./data/gbans')
-			api.sendReply(msg, "ID " ..matches[2].. " *ID no se encuentra en BD*.", true)			
+			api.sendReply(msg, "ID " ..matches[2].. " *ID is not in BD*.", true)			
 			bot_init(true)
 		end
 
@@ -54,10 +54,10 @@ if matches[1] == "isban" then
 	 if not matches[2] then
 	 	if msg.reply then
 			os.execute('grep -c "' ..msg.reply.from.id.. '," ./data/gbans')
-			api.sendReply(msg, "ID " ..msg.reply.from.id.. " *ID se encuentra en BD*.", true)
+			api.sendReply(msg, "ID " ..msg.reply.from.id.. " *ID is in BD*.", true)
 			bot_init(true)
 		 else
-		api.sendMessage(msg.chat.id, "Este comando necesita respuesta")
+		api.sendMessage(msg.chat.id, "This command needs to answer")
 	end
 end
 end
